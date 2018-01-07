9ee6628446432546da648310fcca65099e24081e
alexandra-b <alexandra.bocereg@gmail.com>
2018-01-07 22:08:45 +0200

    added nf hooks

--- firewall-km.c
+++ firewall-km.c
@@ -11,13 +11,18 @@
 #include <linux/ip.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
- 
+#include "utils.h"
+
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("hello");
 MODULE_AUTHOR("papagalu");
  
 #define MAX_RULE_LENGTH       PAGE_SIZE
 static struct proc_dir_entry *proc_entry;
+
+//structure used to register the function
+static struct nf_hook_ops nf_hookfunc_in;
+static struct nf_hook_ops nf_hookfunc_out;
 
 struct mf_rule {
     unsigned char inbound_outbound;
@@ -143,6 +148,175 @@ struct file_operations firewall_fops = {
     .write = rule_write
 };
 
+unsigned int hook_func_in(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *)){
+  struct iphdr *ip_hdr = (struct iphdr *)skb_network_header(skb);
+  struct udphdr *udp_hdr;
+  struct tcphdr *tcp_hdr;
+  struct list_head *p;
+  struct mf_rule *rule_found;
+  char src_ip_str[16],dest_ip_str[16];
+  int i=0;
+  unsigned int src_ip = (unsigned int) ip_hdr->saddr;
+  unsigned int dest_ip = (unsigned int) ip_hdr->daddr;
+  unsigned int src_port,dest_port;
+  if (ip_hdr->protocol==17){
+    udp_hdr = (struct udphdr *)(skb_transport_header(skb)+20);
+    src_port = (unsigned int)ntohs(udp_hdr->source);
+    dest_port = (unsigned int)ntohs(udp_hdr->dest);
+  }else if (ip_hdr->protocol==6){
+    tcp_hdr = (struct tcphdr *)(skb_transport_header(skb)+20);
+    src_port = (unsigned int)ntohs(tcp_hdr->source);
+    dest_port = (unsigned int)ntohs(tcp_hdr->dest);
+  }
+  strncpy(src_ip_str,ip_int_to_str(src_ip),15);
+  strncpy(dest_ip_str,ip_int_to_str(dest_ip),15);
+  printk(KERN_INFO "IN packet info src ip: %u = %s, src port: %u; dest ip: %u = %s, dest port: %u; protocol: %u\n",src_ip,src_ip_str,src_port,dest_ip,dest_ip_str,dest_port,ip_hdr->protocol);
+
+  list_for_each(p,&policy_list.list){
+    i++;
+    rule_found = list_entry(p, struct mf_rule, list);
+    if(rule_found->inbound_outbound!=0){
+      printk(KERN_INFO "rule %d (rule_found->inbound_outbound:%u) not matched: in packet, rule does not specify as in\n",i,rule_found->inbound_outbound);
+      continue;
+    }else{
+      //check the protocol
+      if((rule_found->protocol==1)&&(ip_hdr->protocol!=6)){
+	printk(KERN_INFO "rule %d not matched: rule-TCP, but packet is not TCP\n",i);
+	continue;
+      }else if ((rule_found->protocol==2)&&(ip_hdr->protocol!=17)){
+	printk(KERN_INFO "rule %d not matched: rule-UDP but packet is not UDP\n",i);
+	continue;
+      }
+      if(rule_found->source_ip==0){
+	//............source ip not specified => match...........
+      }else{
+	if(!check_ip_integrity(src_ip,rule_found->source_ip,rule_found->source_netmask)){
+	  printk(KERN_INFO "rule %d not matched: src ip mismatch\n",i);
+	  continue;
+	}
+      }
+      if(rule_found->destination_ip==0){
+	//...............destination ip not specified => match............
+      }else{
+	if(!check_ip_integrity(dest_ip,rule_found->destination_ip,rule_found->destination_netmask)){
+	  printk(KERN_INFO "rule %d not matched: dest ip mismatch\n",i);
+	  continue;
+	}
+      }
+      if(rule_found->source_port==0){
+	//..............source port not specified => match..............
+      }else if (src_port!=rule_found->source_port){
+	printk(KERN_INFO "rule %d not matched: source port mismatch\n",i);
+	continue;
+      }
+      if(rule_found->destination_port==0){
+	//..........destination port not specified => match............
+      }else if(dest_port!=rule_found->destination_port){
+	printk(KERN_INFO "rule %d not matched: destination port mismatch\n",i);
+	continue;
+      }
+      //match found check action to be taken
+      if(rule_found->action==0){
+	printk(KERN_INFO "Match found: %d, packet is dropped\n",i);
+	return NF_DROP;
+      }
+      else{
+	printk(KERN_INFO "Match found: %d, packet accepted\n",i);
+	return NF_ACCEPT;
+      }
+    }
+	
+  }
+  printk(KERN_INFO "No matching found, packet is accepted\n");
+  return NF_ACCEPT;
+}
+
+unsigned int hook_func_out(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *)){
+  struct iphdr *ip_hdr = (struct iphdr *)skb_network_header(skb);
+  struct udphdr *udp_hdr;
+  struct tcphdr *tcp_hdr;
+  struct list_head *p;
+  struct mf_rule *rule_found;
+  char src_ip_str[16],dest_ip_str[16];
+    int i=0;
+  unsigned int src_ip = (unsigned int) ip_hdr->saddr;
+  unsigned int dest_ip = (unsigned int) ip_hdr->daddr;
+  unsigned int src_port,dest_port;
+  if (ip_hdr->protocol==17){
+    udp_hdr = (struct udphdr *)skb_transport_header(skb);
+    src_port = (unsigned int)ntohs(udp_hdr->source);
+    dest_port = (unsigned int)ntohs(udp_hdr->dest);
+  }else if (ip_hdr->protocol==6){
+    tcp_hdr = (struct tcphdr *)skb_transport_header(skb);
+    src_port = (unsigned int)ntohs(tcp_hdr->source);
+    dest_port = (unsigned int)ntohs(tcp_hdr->dest);
+  }
+  strncpy(src_ip_str,ip_int_to_str(src_ip),15);
+  strncpy(dest_ip_str,ip_int_to_str(dest_ip),15);
+  printk(KERN_INFO "OUT packet info src ip: %u = %s, src port: %u; dest ip: %u = %s, dest port: %u; protocol: %u\n",src_ip,src_ip_str,src_port,dest_ip,dest_ip_str,dest_port,ip_hdr->protocol);
+  
+  list_for_each(p,&policy_list.list){
+    i++;
+    rule_found = list_entry(p, struct mf_rule, list);
+    if(rule_found->inbound_outbound!=1){
+      printk(KERN_INFO "rule %d (rule_found->inbound_outbound:%u) not matched: in packet, rule does not specify as out\n",i,rule_found->inbound_outbound);
+      continue;
+    }else{
+      //check the protocol
+      if((rule_found->protocol==1)&&(ip_hdr->protocol!=6)){
+	printk(KERN_INFO "rule %d not matched: rule-TCP, but packet is not TCP\n",i);
+	continue;
+      }else if ((rule_found->protocol==2)&&(ip_hdr->protocol!=17)){
+	printk(KERN_INFO "rule %d not matched: rule-UDP but packet is not UDP\n",i);
+	continue;
+      }
+      if(rule_found->source_ip==0){
+	//..............source ip not specified => match...................
+      }else{
+	//check ip
+	if(!check_ip_integrity(src_ip,rule_found->source_ip,rule_found->source_netmask)){
+	  printk(KERN_INFO "rule %d not matched: src ip mismatch\n",i);
+	  continue;
+	}
+      }
+      if(rule_found->destination_ip==0){
+	//..............destination ip not specified => match..............
+      }else{
+	if(!check_ip_integrity(dest_ip,rule_found->destination_ip,rule_found->destination_netmask)){
+	  printk(KERN_INFO "rule %d not matched: dest ip mismatch\n",i);
+	  continue;
+	}
+      }
+      if(rule_found->source_port==0){
+	//...........source port not specified => match................
+      }else if (src_port!=rule_found->source_port){
+	printk(KERN_INFO "rule %d not matched: source port mismatch\n",i);
+	continue;
+      }
+      if(rule_found->destination_port==0){
+	//.............destination port not specified => match................
+      }else if(dest_port!=rule_found->destination_port){
+	printk(KERN_INFO "rule %d not matched: destination port mismatch\n",i);
+	continue;
+      }
+      //match found check action to be taken
+      if(rule_found->action==0){
+	printk(KERN_INFO "Match found: %d, packet is dropped\n",i);
+	return NF_DROP;
+      }
+      else{
+	printk(KERN_INFO "Match found: %d, packet accepted\n",i);
+	return NF_ACCEPT;
+      }
+    }
+	
+  }
+  printk(KERN_INFO "No matching found, packet is accepted\n");
+  return NF_ACCEPT;
+  
+
+}
+
 int init_firewall_module( void ) {
  
     int ret = 0;
@@ -159,8 +333,21 @@ int init_firewall_module( void ) {
         } else {
             INIT_LIST_HEAD(&(policy_list.list));
             printk(KERN_INFO "firewall: Module loaded.\n");
+	    //hook structure for incoming packet hook
+	    nf_hookfunc_in.hook= hook_func_in;
+	    nf_hookfunc_in.hooknum=NF_INET_LOCAL_IN;
+	    nf_hookfunc_in.pf=PF_INET;
+	    nf_hookfunc_in.priority=NF_IP_PRI_FIRST;
+	    nf_register_hook(&nf_hookfunc_in);
+	    //hook structure for outgoing packet hook
+	    nf_hookfunc_out.hook = hook_func_out;
+	    nf_hookfunc_out.hooknum=NF_INET_LOCAL_OUT;
+	    nf_hookfunc_out.pf=PF_INET;
+	    nf_hookfunc_out.priority=NF_IP_PRI_FIRST;
+	    nf_register_hook(&nf_hookfunc_out);
         }
     }
+    
     return ret;
 }
  
@@ -168,6 +355,8 @@ void cleanup_firewall_module( void ) {
     struct list_head *p, *q;
     struct mf_rule *rule_u;
     vfree(rule_buffer);
+    nf_unregister_hook(&nf_hookfunc_in);
+    nf_unregister_hook(&nf_hookfunc_out);
     list_for_each_safe(p, q, &policy_list.list) {
         printk(KERN_INFO "free one\n");
         rule_u = list_entry(p, struct mf_rule, list);
